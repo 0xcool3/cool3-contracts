f8ee7263ded3e9fa6bbcd3ecb6559c040d9e7435
shelchin <hi@shelchin.com>
2024-05-26 11:50:38 +0800

    Updage treasury package.json

--- packages/treasury/hardhat.config.ts
+++ packages/treasury/hardhat.config.ts
@@ -1,8 +1,34 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import "xdeployer";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-verify";
+import {
+  customChains,
+  networksList,
+  rpcUrlsList,
+  rpcUrlsMap,
+} from "./network.config.ts";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: rpcUrlsMap,
+  xdeploy: {
+    contract: "TokenLock",
+    salt: "cool3.eth",
+    signer: vars.get("PRIVATE_KEY", ""),
+    networks: networksList,
+    rpcUrls: rpcUrlsList,
+    gasLimit: 1_500_000, // optional; default value is `1.5e6`
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: vars.get("API_KEY", ""),
+    customChains,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
 
 export default config;
